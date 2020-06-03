09da64e8398ab73a683ae82eb4eddb78a394f809
Hassan Kibirige <has2k1@gmail.com>
2020-06-03 22:50:07 +0300

    MAINT: Remove unused case from datetime_trans
    When the datetime_trans.transform was written `date2num`
    from matplotlib did not support `np.datetime64` so we had
    a special case for it. Now `date2num` has `np.datetime64`
    support.

--- mizani/transforms.py
+++ mizani/transforms.py
@@ -605,16 +605,7 @@ class datetime_trans(trans):
         """
         Transform from date to a numerical format
         """
-        try:
-            x = date2num(x)
-        except AttributeError:
-            # numpy datetime64
-            # This is not ideal because the operations do not
-            # preserve the np.datetime64 type. May be need
-            # a datetime64_trans
-            x = [pd.Timestamp(item) for item in x]
-            x = date2num(x)
-        return x
+        return date2num(x)
 
     @staticmethod
     def inverse(x):

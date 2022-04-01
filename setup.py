0db0e0e28d1b7dac85263d83852d754b69fe553f
Hassan Kibirige <has2k1@gmail.com>
2022-04-01 15:48:56 +0300

    MAINT: Les setup.py use pyproject.toml & setup.cfg
    When pytest config is moved to pyproject.toml, pytest test
    discovery changes and it catches a bug in MPL <=3.5.1.
    We shall make this change well after MPL 3.5.2 has shipped.
    
    Ref: https://github.com/matplotlib/matplotlib/issues/22305

--- setup.py
+++ setup.py
@@ -1,83 +1,15 @@
-"""
-Mizani
+import sys
+from setuptools import setup
 
-Mizani is a scales package for graphics. It is based on Hadley
-Wickham's *Scales* package.
-"""
-
-from setuptools import setup, find_packages
-
-import versioneer
+# This appears to be necessary so that versioneer works
+# Ref: python-versioneer/python-versioneer/issues/193
+sys.path.insert(0, ".")  # noqa
+import versioneer        # noqa
 
 
-__author__ = 'Hassan Kibirige'
-__email__ = 'has2k1@gmail.com'
-__description__ = "Scales for Python"
-__license__ = 'BSD (3-clause)'
-__url__ = 'https://github.com/has2k1/mizani'
-__classifiers__ = [
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3',
-    'Topic :: Scientific/Engineering :: Visualization',
-]
-
-
-def check_dependencies():
-    """
-    Check for system level dependencies
-    """
-    pass
-
-
-def get_required_packages():
-    """
-    Return required packages
-
-    Plus any version tests and warnings
-    """
-    install_requires = ['numpy',
-                        'scipy >= 1.5.0',
-                        'pandas >= 1.1.0',
-                        'matplotlib >= 3.1.1',
-                        'palettable']
-    return install_requires
-
-
-def get_package_data():
-    """
-    Return package data
-
-    For example:
-
-        {'': ['*.txt', '*.rst'],
-         'hello': ['*.msg']}
-
-    means:
-        - If any package contains *.txt or *.rst files,
-          include them
-        - And include any *.msg files found in
-          the 'hello' package, too:
-    """
-    return {}
-
-
-if __name__ == '__main__':
-
-    check_dependencies()
-
-    setup(name='mizani',
-          maintainer=__author__,
-          maintainer_email=__email__,
-          description=__description__,
-          long_description=__doc__,
-          license=__license__,
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          url=__url__,
-          python_requires='>=3.6',
-          install_requires=get_required_packages(),
-          packages=find_packages(),
-          package_data=get_package_data(),
-          classifiers=__classifiers__
-          )
+setup(
+    # Move these to setup.cfg or pyproject.toml
+    # whenever it is possible to do so
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)

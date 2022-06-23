b6c0a85a3199499b607ebb9ecc057434a9ea2fe5
Hassan Kibirige <has2k1@gmail.com>
2022-06-23 10:46:55 +0300

    Fix version number to check for mizani
    and not plotnine. Copypaste error!

--- mizani/__init__.py
+++ mizani/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import version, PackageNotFoundError
 
 
 try:
-    __version__ = version('plotnine')
+    __version__ = version('mizani')
 except PackageNotFoundError:
     # package is not installed
     pass

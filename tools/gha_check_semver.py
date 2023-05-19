24f0d46c3c32d7aebd816624362fa8db80fa349c
Hassan Kibirige <has2k1@gmail.com>
2023-05-19 10:17:17 +0300

    Publish with Github Actions

---
+++ tools/gha_check_semver.py
@@ -0,0 +1,109 @@
+"""
+Check the Tag on Github Actions
+
+Writes the following variables to the output file for the github step:
+
+    1. tag_type=<tag_type>
+       Possible values are:
+           - alpha
+           - beta
+           - candidate
+           - final
+           - ""
+
+    2. publish_index=<publish_index>
+       Possible values are:
+           - pypi
+           - testpypi
+           - ""
+
+The output of this script should tell you whether to publish a release
+and to what index.
+"""
+import os
+import re
+from typing import Literal, TypeAlias
+
+TagType: TypeAlias = Literal[
+    "alpha",
+    "beta",
+    "candidate",
+    "final",
+]
+
+
+count = r"(?:[0-9]|[1-9][0-9]+)"
+SEMVER_PATTERN = re.compile(
+    r"^v"
+    rf"(?P<version>{count}\.{count}\.{count})"
+    rf"(?P<pre>(a|b|rc){count})?"
+    r"$"
+)
+
+
+def get_tag_type() -> TagType | None:
+    """
+    Return the type of semantic version
+    """
+    tag = os.environ.get("GITHUB_REF_NAME")
+
+    # Probably not on GHA
+    if not tag:
+        return
+
+    m = SEMVER_PATTERN.match(tag)
+    if not m:
+        return
+
+    pre = m.group("pre")
+
+    if pre:
+        if pre == "a":
+            return "alpha"
+        elif pre == "b":
+            return "beta"
+        else:
+            return "candidate"
+
+    return "final"
+
+
+def output_tag_type(tag_type: TagType | None):
+    """
+    Write tag_type to the GHA output
+    """
+    output_file = os.environ.get("GITHUB_OUTPUT")
+
+    # Probably not on GHA
+    if not output_file:
+        return
+
+    with open(output_file, "a") as f:
+        print(f"tag_type={tag_type}", file=f)
+
+
+def output_publish_index(tag_type: TagType | None):
+    """
+    Write release_type to the GHA output
+    """
+    output_file = os.environ.get("GITHUB_OUTPUT")
+
+    # Probably not on GHA
+    if not output_file:
+        return
+
+    if tag_type in ("alpha", "beta", "candidate"):
+        publish_index = "testpypi"
+    elif tag_type == "final":
+        publish_index = "pypi"
+    else:
+        publish_index = ""
+
+    with open(output_file, "a") as f:
+        print(f"publish_index={publish_index}", file=f)
+
+
+if __name__ == "__main__":
+    tag_type = get_tag_type()
+    output_tag_type(tag_type)
+    output_publish_index(tag_type)
